d0b8af26c5139e2e9f73da8403265217278ba6e7
marigold-95 <97016865+marigold-95@users.noreply.github.com>
2024-01-18 17:49:31 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ComboBox1 = New ComboBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"A", "A-", "B", "C", "D"})
+        ComboBox1.Location = New Point(455, 66)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 23)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(322, 69)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(88, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter the Grade"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(322, 112)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(44, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Results"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(455, 104)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(282, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(316, 185)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 24)
+        Button1.TabIndex = 4
+        Button1.Text = "Compute!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ScrollBar
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
